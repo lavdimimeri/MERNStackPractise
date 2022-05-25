36ce9a8927df033da638439022dc1295cc2549d2
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-05-25 19:54:07 +0200

    javascript variables and strings

---
+++ Step34-JavaScript/javascript/learn-to-code.js
@@ -0,0 +1,11 @@
+//var name="Jack";
+//var age="23";
+
+
+var firstName="John";
+var lastName="Smith";
+var dateOfBirth="10-09-2000";
+
+var message="Welcome, "+firstName;      
+console.log(message);
+    
