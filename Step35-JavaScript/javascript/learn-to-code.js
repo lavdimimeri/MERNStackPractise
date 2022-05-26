431efbd8e94de7e9ec2ad0ed6de6af5a9eceb31b
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-05-26 23:39:36 +0200

    very basic math operators recap.

---
+++ Step35-JavaScript/javascript/learn-to-code.js
@@ -0,0 +1,19 @@
+//var name="Jack";
+//var age="23";
+
+
+//var firstName="John";
+//var lastName="Smith";
+//var dateOfBirth="10-09-2000";
+
+//var message="Welcome, "+firstName;      
+//console.log(message);
+    
+var sum=10+15;
+var sub=15-10;
+var mul=10*3;
+var div=10/3;
+var mod=10%3;
+
+var result=10*(5-2);
+console.log(result);
