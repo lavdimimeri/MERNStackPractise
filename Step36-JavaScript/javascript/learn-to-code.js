967374262a5c60e8956cf7538ae7f2519a73ace0
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-05-27 00:03:02 +0200

    If else statements

---
+++ Step36-JavaScript/javascript/learn-to-code.js
@@ -0,0 +1,46 @@
+//var name="Jack";
+//var age="23";
+
+
+//var firstName="John";
+//var lastName="Smith";
+//var dateOfBirth="10-09-2000";
+
+//var message="Welcome, "+firstName;      
+//console.log(message);
+    
+//var sum=10+15;
+//var sub=15-10;
+//var mul=10*3;
+//var div=10/3;
+//var mod=10%3;
+
+//var result=10*(5-2);
+//console.log(result);
+
+
+var myAccountBalance=300;
+var nikeShoes=799.23;
+var coupon=500;
+
+var val1=23:
+var val2="23";
+
+//if(nikeShoes<=myAccountBalance){
+//    myAccountBalance=myAccountBalance-nikeShoes-coupon;
+ //   console.log("we just bought some shoes");
+//    console.log("Account Balance: " + myAccountBalance);
+//}else if(nikeShoes-coupon<=myAccountBalance){
+  //     console.log("we just bought some shoes");
+    //console.log("Account Balance: " + myAccountBalance);
+//}
+
+//else{
+//    console.log("Sorry you do not have enough money");
+//}
+
+if(val1===val2){
+    console.log("I am same age as John");
+}else{
+    console.log("Not the same");
+}
