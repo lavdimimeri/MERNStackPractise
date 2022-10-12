ce93fed8152d77fd70122891f4f0e988472b348d
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-12 16:35:24 +0200

    Started with react

---
+++ React/beer-table/src/App.js
@@ -0,0 +1,19 @@
+
+import { useState } from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title= 'Welcome to the page';
+  return (
+    <div className="App">
+      <Navbar />
+      <div className='content'>
+       <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
