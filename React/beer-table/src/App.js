51e0537b7e6869f0d211dcaf2b1453afe56b6a74
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-13 15:01:53 +0200

    PLaying around with hooks(useState) and buttons

--- React/beer-table/src/App.js
+++ React/beer-table/src/App.js
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import './App.css';
+
 import Navbar from './Navbar';
 import Home from './Home';
 
--- React/beer-table/src/Home.js
+++ React/beer-table/src/Home.js
@@ -1,8 +1,25 @@
+import React from "react";
+import { useState } from 'react'
 const Home = () => {
+const [name, setName]=useState('Mario');
+const [age, setAge]=useState('25');
+
+const handleClick = () => {
+  setName('Lavdim');
+  setAge(27);
+}
+const handleClickAgain=(name) => {
+  console.log(name);
+}
+
+
+
     return (
        <div className="home">
          <h2>Home page</h2>
-         
+         <p>{name} is {age} old </p>
+         <button onClick={handleClick}>Click Me!</button>
+         <button onClick={() => handleClickAgain(name)}>Click me again</button>
        </div>
 
       );
