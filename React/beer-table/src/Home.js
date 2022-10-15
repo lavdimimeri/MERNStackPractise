85e14c37f54dbe0bccd7fb6703c4c03fa59c7bf2
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-15 16:16:41 +0200

    sending filtered data as props from parent component to child component

--- React/beer-table/src/Home.js
+++ React/beer-table/src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
 const[blogs, setBlogs] = useState([
   {title: 'My new Website', body:'lorem ipsum...', author:'lavdim1',src:'/images/3408.jpeg', id:1},
   {title: 'Welcome party', body:'lorem ipsum...', author:'lavdim2',src:'/images/3408.jpeg', id:2},
-  {title: 'Web dev top tips', body:'lorem ipsum...', author:'lavdim3',src:'/images/3408.jpeg',id:3}
+  {title: 'Web dev top tips', body:'lorem ipsum...', author:'lavdim1',src:'/images/3408.jpeg',id:3}
 ]);
 
     return (
        <div className="home">
         <BlogList blogs={blogs} title='These are my blogs'/>
+        <BlogList blogs={blogs.filter((blog) => blog.author ==='lavdim1')} title='Lavdim 1 blogs'/>
         </div>
 
       );
