792e22a26328916beae52f8dd2e916551b87edd9
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-19 20:34:51 +0200

    Handling fetch errors

--- React/beer-table/src/Home.js
+++ React/beer-table/src/Home.js
@@ -4,8 +4,9 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
+ /* useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json();
@@ -16,22 +17,30 @@ const Home = () => {
         setIsPending(false);
       });
   }, [])
-
+*/
     //------ SETING TIMOUT IN ORDER TO NOTICE THE LOADING DIV
-  /*useEffect(() => {
+  useEffect(() => {
     setTimeout(() => {
      fetch('http://localhost:8000/blogs')
      .then(res => {
+      if(!res.ok){
+          throw Error('Could not fetch data');
+      }
        return res.json();
      })
      .then(data => {
         console.log(data);
        setBlogs(data);
        setIsPending(false);
-     });
+       setError(null);
+     })
+     .catch(err => {
+      setError(err.message);
+      setIsPending(false);
+     })
     },1000)
    }, [])
-  */
+  
      // ----FETCHING DATA USING ASYNC/AWAIT (MODERN WAY OF ) 
    /* useEffect(() => {
       fetchData();
@@ -52,6 +61,7 @@ const Home = () => {
 */
   return (
     <div className="home">
+      {error && <div>{ error }</div>}
       {isPending && <div>Loading....</div>}
       {blogs && <BlogList blogs={blogs} title={'All blogs'}/>}
     </div>
