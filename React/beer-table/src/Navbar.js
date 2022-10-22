8636c08e8eaed89236e5db50c8178194e6f8c6f6
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-22 16:08:50 +0200

    Router Links

--- React/beer-table/src/Navbar.js
+++ React/beer-table/src/Navbar.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () =>{
     const title='The Dojo Blog';
     return (
         <nav className="navbar">
             <h1>{title}</h1>
             <div className="links">
-               <a href="/">Home</a>
-               <a href="/create">New Blog</a>
+               <Link to="/">Home</Link>
+               <Link to="/create">New Blog</Link>
 
             </div>
         </nav>
