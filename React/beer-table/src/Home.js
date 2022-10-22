17b0aa67ea3217d75d82725b693d8c8c7b98c024
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-22 16:34:28 +0200

    Cleanup useEffect

--- React/beer-table/src/Home.js
+++ React/beer-table/src/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
--- React/beer-table/src/useFetch.js
+++ React/beer-table/src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
  
     useEffect(() => {
+        const abortCont=new AbortController();
         setTimeout(() => {
-         fetch(url)
+         fetch(url, {signal: abortCont.signal})
          .then(res => {
           if(!res.ok){
               throw Error('Could not fetch data');
@@ -21,10 +22,17 @@ const useFetch = (url) => {
            setError(null);
          })
          .catch(err => {
-          setError(err.message);
-          setIsPending(false);
+            if(err.name === 'AbortError'){
+                 console.log('FETCH ABORTED')
+            }else{
+                setError(err.message);
+                setIsPending(false);
+            }
+         
          })
         },1000)
+        //return () => console.log('Cleanup')
+        return () => abortCont.abort();
        }, [url])
    
        return {data, isPending, error}
