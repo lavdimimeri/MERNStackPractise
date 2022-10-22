6ace5c5551da15ad3a36452f99dc5a842836ad8b
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-22 15:55:36 +0200

    exact match routes

--- React/beer-table/src/App.js
+++ React/beer-table/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
 
 function App() {
   const title= 'Welcome to the page';
@@ -13,9 +14,14 @@ function App() {
       <Navbar />
       <div className='content'>
        <Switch>
-        <Route path="/">
-          <Home/>
+        <Route exact path="/">
+          <Home />
           </Route>
+
+          <Route path="/create">
+          <Create />
+          </Route>
+
        </Switch>
       </div>
     </div>
