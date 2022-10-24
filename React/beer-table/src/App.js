7d42993294f57d04e274f86a0f80bd7d034ec13a
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-10-24 23:26:03 +0200

    route parameters

--- React/beer-table/src/App.js
+++ React/beer-table/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   const title= 'Welcome to the page';
@@ -20,6 +21,10 @@ function App() {
 
           <Route path="/create">
           <Create />
+          </Route>
+
+          <Route path="/blogs/:id">
+          <BlogDetails />
           </Route>
 
        </Switch>
