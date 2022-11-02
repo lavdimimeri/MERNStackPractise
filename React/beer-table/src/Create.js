0f1d6f7e2320108af77c7a995ee282d7e15ed6fd
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-11-02 16:11:48 +0100

    programmatic redirects using useHistory() Hook

--- React/beer-table/src/Create.js
+++ React/beer-table/src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
    const[title, setTitle] = useState('');
    const[body, setBody] = useState('');
    const[author, setAuthor ] = useState('mario');
    const[isPending, setIsPending] = useState(false);
+   const history = useHistory();
 
    const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +22,8 @@ const Create = () => {
     }).then(() => {
       console.log('A new blog added');
       setIsPending(false);
+      //history.go(-1);
+      history.push('/')
     })
    }
 
