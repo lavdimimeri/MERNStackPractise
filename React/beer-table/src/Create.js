e8d55c74935160c0f058d819d73a956be3c10d00
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2022-11-02 15:29:21 +0100

    Submit events

--- React/beer-table/src/Create.js
+++ React/beer-table/src/Create.js
@@ -5,11 +5,19 @@ const Create = () => {
    const[body, setBody] = useState('');
    const[author, setAuthor ] = useState('mario');
 
+   const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blog ={title, body, author};
+    
+    console.log(blog);
+   }
+
 
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
           <label>Blog Title:</label>
           <input
            type="text"
